72cb925126ccbaf639bbbabb5112a816145ad020
arrg <arrg@lorrgs.io>
2025-06-21 14:26:49 +0100

    add font to LibSharedMedia

--- fonts.lua
+++ fonts.lua
@@ -4,8 +4,12 @@ local ADDON_NAME = ...
 ---@class AddonNamespace
 local NS = select(2, ...)
 
+local LSM = LibStub("LibSharedMedia-3.0")
+
 
 local FONT_NAME = "Interface/Addons/" .. ADDON_NAME .. "/media/fonts/Cabin.ttf"
+
+LSM:Register("font", "Cabin", FONT_NAME)
 
 
 _G.UNIT_NAME_FONT = FONT_NAME
