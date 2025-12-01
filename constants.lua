a454aa3d39b6fda3370f6947be8b90805b810ed0
arrg <arrg@lorrgs.io>
2025-12-01 16:19:29 +0000

    fix: use ColorObjects

--- constants.lua
+++ constants.lua
@@ -1,7 +1,7 @@
 
 
 
-FACTION_BAR_COLORS[7] = {r=0.0, g=1.0, b=0.75}
-FACTION_BAR_COLORS[8] = {r=0.75, g=0.75, b=0.75}
+FACTION_BAR_COLORS[7] = CreateColor(0.0, 1.0, 0.75)
+FACTION_BAR_COLORS[8] = CreateColor(0.75, 0.75, 0.75)
 
 
